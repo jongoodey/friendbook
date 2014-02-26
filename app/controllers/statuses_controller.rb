727193b8315845c7de779fd1c9eaf36c9a1f937d
Jon Goodey <goodeyjon@hotmail.com>
2014-02-26 14:16:03 +0000

    changes to the main login added method for full name and sign out button

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,9 +1,10 @@
 class StatusesController < ApplicationController
   # GET /statuses
   # GET /statuses.json
+  
   def index
     @statuses = Status.all
-
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @statuses }
